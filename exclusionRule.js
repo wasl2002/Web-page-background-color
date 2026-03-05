ac4c9dcfc6a5051b87f4527c7e14eb9735b494c5
GW <35213327+wasl2002@users.noreply.github.com>
2026-03-05 10:04:51 +0800

    Add exclusion rule for Zhihu header

--- exclusionRule.js
+++ exclusionRule.js
@@ -3,6 +3,11 @@
     className: 'bmc-widgets singleEntityWrapper_srJlN',
     excludeChildren: true,
     reason: 'bing地图排除'
+  }，
+  {
+    className: 'css-s8xum0',
+    excludeChildren: false,
+    reason: '知乎header'
   }
 ]
      
